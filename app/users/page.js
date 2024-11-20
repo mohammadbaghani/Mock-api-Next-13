bdd302ee1718b352f835cdead3655c096dd515be
https://github.com/mohammadbaghani <baghamoh3@gmail.com>
2024-11-20 17:34:15 +0330

    Baghani codes sended to git

---
+++ app/users/page.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+
+
+export default function Users() {
+
+   
+
+        let { id, title, price, img } = props
+
+        return (<>
+
+
+            <Link to={`/e/${id}`}>
+
+                <Card style={{ width: '13rem' }} className='image'>
+
+                    <Card.Img variant="top" className='imageorigin' src={img} style={{ height: '11rem' }} />
+
+                    <Card.Body style={{ textDecoration: 'none' }}>
+                        <Card.Title className='title' id='title' style={{ textDecoration: 'none' }}>{title}</Card.Title>
+
+                        <Card.Text className='price'>
+                            {price}
+                        </Card.Text>
+
+                        <Button className='details'>
+                            مشاهده
+                            جزئیات
+                        </Button>
+
+                    </Card.Body>
+
+                </Card>
+
+            </Link>
+            
+        </>
+        )
+    
+}
